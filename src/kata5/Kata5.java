de14f60d29710e73687823c02034a86c1caa8205
Naim <Naim@DESKTOP-CBOOKB5>
2023-01-03 18:00:18 +0000

    Second version of kata5

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -9,9 +9,7 @@ import java.sql.Statement;
 public class Kata5 {
 
     public static void main(String[] args) {
-        //connect();
-        Kata5 kata = new Kata5();
-        kata.selectAll();
+        createNewTable();
     }
 
     private Connection connect() {
@@ -50,4 +48,18 @@ public class Kata5 {
         }
     }
     
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:kata5.db";
+        
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+            + " Id integer PRIMARY KEY AUTOINCREMENT,\n"
+            + " Mail text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage()); 
+        }
+    }
 }
